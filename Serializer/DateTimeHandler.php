0b06d49f5907321e03c088e53a0d91ab9cb8a251
Vyacheslav Salakhutdinov <megazoll@gmail.com>
2016-03-24 21:50:45 +0000

    Remove unused use statement

--- Serializer/DateTimeHandler.php
+++ Serializer/DateTimeHandler.php
@@ -2,8 +2,6 @@
 
 namespace Staffim\DateTimeBundle\Serializer;
 
-use JMS\DiExtraBundle\Annotation as DI;
-
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
 use JMS\Serializer\GraphNavigator;
 use JMS\Serializer\JsonSerializationVisitor as JmsJsonSerializationVisitor;
